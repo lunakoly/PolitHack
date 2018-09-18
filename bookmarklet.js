0f863203fa626402f7ac6376e0e470dbf2aada81
Nick <lunyak.kolya@mail.ru>
2018-09-18 20:07:31 +0300

    + db + coloring 2

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,30 +1,34 @@
 javascript:(function(){
-	function label(tasks) {
+	function label(target, color, text) {
+		target.innerHTML += '<span style="border-radius:3px;margin-left:5px;background:#' + color + ';padding:3px 7px;">' + text + '</span>';
+	}
+
+	function addLabels(tasks) {
 		const qtexts = document.getElementsByClassName('qtext');
 
 		for (let it of qtexts) {
 			const header = it.firstChild;
-			console.log('==================================');
-			console.log('Testing [' + header.innerText + ']');
+			let found = false;
 
 			for (let task of tasks) {
-				console.log('For [' + task.question + ']');
 				if (task.question == header.innerText) {
-					console.log('Accepted');
-
-					if (task.answers.length == 0) {
-						header.innerHTML += '<span style="border-radius:3px;margin-left:5px;background:#ffa3a3;padding:3px 7px;">No info(</span>';
-					} else {
-						const color = task.hasMistakes ? "feffa3" : "a3c8ff";
-						header.innerHTML += '<span style="border-radius:3px;margin-left:5px;background:#' + color + ';padding:3px 7px;">' + task.answers.join(', ') + '</span>';
-					}
+					if (task.answers.length == 0)
+						label(header, 'feffa3', 'No answers (');
+					else if (task.hasMistakes)
+						label(header, 'feffa3', task.answers.join(', '));
+					else
+						label(header, 'a3c8ff', task.answers.join(', '));
+					found = true;
 				}
 			}
+
+			if (!found)
+				label(header, 'ffa3a3', 'No info(')
 		}
 	}
 
 	fetch('https://raw.githubusercontent.com/lunakoly/PolitHack/master/polit.json')
 	    .then(res => res.json())
-	    .then(label)
+	    .then(addLabels)
 	    .catch(err => console.error(err));
 })()
