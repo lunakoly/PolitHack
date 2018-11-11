b4eb9b0b432d99b6999a8bccd342e5d317e15410
Nick <lunyak.kolya@mail.ru>
2018-11-12 00:49:27 +0300

    REDESIGN

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,16 +1,13 @@
 javascript:(function(){
-	fetch('https://raw.githubusercontent.com/lunakoly/PolitHack/debug/hack.js')
-		.then(res => res.text())
-		.then(it => {
-			const script = document.createElement('script');
-			script.type = 'text/javascript';
-			script.src = it;
-			document.body.appendChild(script);
-			
-			fetch('https://raw.githubusercontent.com/lunakoly/PolitHack/debug/polit.json')
-			    .then(res => res.json())
-			    .then(tasks => PolitHack.labelAll(tasks))
-			    .catch(err => console.error(err));
-		})
-		.catch(err => console.err(err));
+	const script = document.createElement('script');
+	script.type = 'text/javascript';
+	script.src = 'https://cdn.jsdelivr.net/gh/lunakoly/PolitHack@a356026c7269fe38d5735082c536bce2cb4ed1eb/hack.js';
+	document.body.appendChild(script);
+
+	script.onload = e => {
+		fetch('https://raw.githubusercontent.com/lunakoly/PolitHack/debug/polit.json')
+			.then(res => res.json())
+			.then(tasks => PolitHack.labelAll(tasks))
+			.catch(err => console.error(err));
+	};
 })()
