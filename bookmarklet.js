424ce3ef579804f57af723e6041021307f2af1db
Nick <lunyak.kolya@mail.ru>
2018-12-03 00:57:42 +0300

    Testing new structure

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,11 +1,11 @@
 javascript:(function(){
 	const script = document.createElement('script');
 	script.type = 'text/javascript';
-	script.src = 'https://cdn.jsdelivr.net/gh/lunakoly/PolitHack@a356026c7269fe38d5735082c536bce2cb4ed1eb/hack.js';
+	script.src = 'https://cdn.jsdelivr.net/gh/lunakoly/PolitHack/hack.js';
 	document.body.appendChild(script);
 
 	script.onload = e => {
-		fetch('https://raw.githubusercontent.com/lunakoly/PolitHack/debug/polit.json')
+		fetch('https://raw.githubusercontent.com/lunakoly/PolitHack/master/polit.json')
 			.then(res => res.json())
 			.then(tasks => PolitHack.labelAll(tasks))
 			.catch(err => console.error(err));
